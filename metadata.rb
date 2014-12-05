edaaf731924657f22b97ab8495eb4abdcd0a696b
Dmitry Batiyevsky <dbat@softserveinc.com>
2014-12-05 15:32:46 +0200

    changed version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Elliot Kendall"
 maintainer_email  "elliot.kendall@ucsf.edu"
 license           "Apache 2.0"
 description       "Installs Shibboleth IdP"
-version           "0.1.3"
+version           "0.1.4"
 
 recipe "shibboleth_idp", "Installs Shibboleth IdP"
 
