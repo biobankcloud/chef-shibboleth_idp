7d1c09889e198963808e11f57569487cd6dfa3be
Dmitry Batiyevsky <dbat@softserveinc.com>
2014-12-05 17:00:16 +0200

    changed IDP version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # Attributes:: default
 #
 
-default["shibboleth_idp"]["version"] = "2.3.8"
+default["shibboleth_idp"]["version"] = "2.4.1"
 default["shibboleth_idp"]["install_dir"] = "/opt/src"
 default["shibboleth_idp"]["installer_dir"] = default["shibboleth_idp"]["install_dir"] + "/shibboleth-identityprovider-" + default["shibboleth_idp"]["version"]
 default["shibboleth_idp"]["installer_archive"] = default["shibboleth_idp"]["install_dir"] + "/shibboleth-identityprovider-" + default["shibboleth_idp"]["version"] + "-bin.zip"
