d3fdcea18c441d0afe8d87d797a8b8e866594474
Dmitry Batiyevsky <dbat@softserveinc.com>
2014-12-09 13:41:50 +0200

    commented out some files which are already there

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Elliot Kendall"
 maintainer_email  "elliot.kendall@ucsf.edu"
 license           "Apache 2.0"
 description       "Installs Shibboleth IdP"
-version           "0.1.6"
+version           "0.1.7"
 
 recipe "shibboleth_idp", "Installs Shibboleth IdP"
 
